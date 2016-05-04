46dd0f42942b459041bf8d34d7197d97d10edfa6
Will Wheeler <willfulbard@gmail.com>
2016-05-03 20:16:01 -0700

    FEAT - Add socket sending via dispatch using redux-socket.io

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -2,7 +2,7 @@
 
 /* Import Dependencies */
 import { createStore, applyMiddleware, compose } from 'redux';
-
+import React from 'react-native';
 
 /* Import Reducer */
 import rootReducer from '../reducers/index.js';
@@ -11,9 +11,22 @@ import rootReducer from '../reducers/index.js';
 import thunk from 'redux-thunk';
 import devTools from 'remote-redux-devtools';
 
+/* socket.io assumes navigator.userAgent is a string, supply a dummy one to make it happy
+ * and because ES6 module imports are hoisted, we can't make the userAgent assignment
+ * in the same file as the react-native and socket.io imports
+ */
+import './UserAgent';
+import io from 'socket.io-client/socket.io';
+const socket = io.connect('http://localhost:1234', {
+  jsonp: false,
+  transports: ['websocket'],
+});
+import createSocketIoMiddleware from 'redux-socket.io';
+const socketIoMiddleware = createSocketIoMiddleware(socket, 'server/');
+
 export default function configureStore(initialState) {
   const enhancer = compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, socketIoMiddleware),
     devTools({
       name: 'Smile Guess',
       realtime: true,
