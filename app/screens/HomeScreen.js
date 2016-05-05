1e14190952e1ec39793585cda0aefaee1b4853ca
Brandon Ramirez <brandon.ramirez@live.com>
2016-05-04 19:08:49 -0700

    FEAT - Add ESDoc configuration. Add docs to gitignore. Add ESDoc plugin to enable ES7 features in documentation.

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -24,7 +24,14 @@ const styles = StyleSheet.create({
   },
 });
 
-const HomeScreen = ({ onJoinGame }) => (
+
+/**
+ * Home Screen is a React functional component.
+ * It provides an entry point for joining a random game.
+ * In the future, it will display a list of available games to join.
+ * @param {{onJoinGame: function()}} onPress event handler for the Join Game button.
+ */
+export const HomeScreen = ({ onJoinGame }) => (
   <View style={styles.container}>
     <View style={styles.buttonContainer}>
       <Button style={styles.button} onPress={onJoinGame}> Join Random Game! </Button>
@@ -36,6 +43,12 @@ HomeScreen.propTypes = {
   onJoinGame: PropTypes.func.isRequired,
 };
 
+/**
+ * HomeScreenContainer is a 'container component' which binds the props and
+ * actions creators of HomeScreen to the store and dispatcher, respectively.
+ * It should be imported in favor of HomeScreen, which is exported only
+ * for documentation purposes.
+ */
 const HomeScreenContainer = connect(
   mapHomeScreen.mapStateToProps,
   mapHomeScreen.mapDispatchToProps
